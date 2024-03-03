26ea07d8b8adec0e5b13ab7d99b42d271fdee4e7
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2024-03-04 00:49:43 +0600

    add damy setion page

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -5,6 +5,7 @@ import ConvertionF from "../ui/footer/ConvertionF";
 import Footer from "../ui/footer/Footer";
 import Offerd from "../ui/Offerd";
 import MiddleBanner from "../../page/home/MiddleBanner";
+import Sestion from "../../page/home/Sestion";
 
 const MainLayout = () => {
   return (
@@ -13,6 +14,7 @@ const MainLayout = () => {
       <Navbar></Navbar>
       <Outlet></Outlet>
       <MiddleBanner></MiddleBanner>
+      <Sestion></Sestion>
       <Offerd></Offerd>
       <ConvertionF></ConvertionF>
       <Footer></Footer>
--- src/page/home/MiddleBanner.tsx
+++ src/page/home/MiddleBanner.tsx
@@ -3,24 +3,24 @@ import imag2 from "../../assets/20240217-_RHN3393.webp";
 
 const MiddleBanner = () => {
   return (
-    <div className="h-screen bg-[#3c3633] flex py-24 ">
-      <div className="h-full flex relative w-1/2">
+    <div className="h-full bg-[#3c3633] flex py-80">
+      <div className="h-full flex w-1/2  relative justify-center items-center h-10/13 bg-red-500">
         <img
-          className="w-[294px] h-92 absolute top-2 left-20 bottom-10 z-10"
+          className=" w-[294px] h-92 absolute  left-5 z-10 top-[-136px]"
           src={imag2}
           alt=""
         />
         <img
-          className="w-[294px] h-92 absolute left-80 bottom-10"
+          className="w-[294px] h-92 absolute  right-20 "
           src={imag1}
           alt=""
         />
       </div>
-      <div className="w-1/2 flex items-center ">
+      <div className="w-1/2 flex items-center">
         <div className="p-5">
           <p className="font-light pb-4 text-white">Black and White</p>
-          <p className="text-white text-4xl font-bold ">
-            timeless minimals for your wardrobe.
+          <p className="text-white text-4xl font-bold">
+            Timeless minimalism for your wardrobe.
           </p>
         </div>
       </div>
